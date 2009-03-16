5e4d28e7249497b76679d47d6ce55aa6f9c77ea3
Ricky Zhou <ricky@fedoraproject.org>
2009-03-16 18:43:56 -0400

    Initial supybot-notify plugin.

---
+++ plugin.py
@@ -0,0 +1,24 @@
+###
+# Copyright (c) 2009, Ricky Zhou
+# All rights reserved.
+#
+#
+###
+
+import supybot.utils as utils
+from supybot.commands import *
+import supybot.plugins as plugins
+import supybot.ircutils as ircutils
+import supybot.callbacks as callbacks
+
+
+class Notify(callbacks.Plugin):
+    """Add the help for "@plugin help Notify" here
+    This should describe *how* to use this plugin."""
+    threaded = True
+
+
+Class = Notify
+
+
+# vim:set shiftwidth=4 tabstop=4 expandtab textwidth=79:
---
+++ test.py
@@ -0,0 +1,14 @@
+###
+# Copyright (c) 2009, Ricky Zhou
+# All rights reserved.
+#
+#
+###
+
+from supybot.test import *
+
+class NotifyTestCase(PluginTestCase):
+    plugins = ('Notify',)
+
+
+# vim:set shiftwidth=4 tabstop=4 expandtab textwidth=79:
